3bafdde668ab12fe5dd5e385bd1fc3d4c7d41de2
Justas <justaslau@gmail.com>
2020-08-14 16:35:39 -0400

    added repo components, completed app with class components

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+const UserItem = ({ user: { login, avatar_url } }) => {
   return (
     <div className="card text-center">
       <img
@@ -12,9 +13,9 @@ const UserItem = ({ user: { login, avatar_url, html_url } }) => {
       />
       <h3>{login}</h3>
       <div>
-        <a className="btn btn-dark btn-sm my-1" href={html_url}>
+        <Link className="btn btn-dark btn-sm my-1" to={`/user/${login}`}>
           More
-        </a>
+        </Link>
       </div>
     </div>
   );
